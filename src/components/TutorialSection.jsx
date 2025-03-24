624face920b4c3c43afa544ff46373cb511f213e
Shahnawaz Ali <shahnawaz132007@gmail.com>
2025-03-25 03:09:11 +0530

    refactor TutorialSection layout for improved responsiveness and spacing

--- src/components/TutorialSection.jsx
+++ src/components/TutorialSection.jsx
@@ -4,13 +4,13 @@ import PrimaryButton from './PrimaryButton'
 function TutorialSection() {
     return (
         <div className='tutorial-section border-b-[1px] border-gray-800 flex tablet:flex-row flex-col tablet:justify-between tablet:items-center tablet:py-10 pb-10 h-fit gap-10 tablet:gap-0'>
-            <div className='flex flex-col gap-10'>
-                <h1 className='tablet:font-bold font-semibold text-(--primary-text) text-5xl tablet:text-7xl tracking-tighter tablet:leading-20'>Looking for unlimited receipts?</h1>
+            <div className='flex flex-col gap-7'>
+                <h1 className='tablet:font-bold font-semibold text-(--primary-text) tablet:w-[33vw] text-5xl tablet:text-7xl tracking-tighter tablet:leading-20'>Looking for unlimited receipts?</h1>
                 <p className='tablet:text-2xl text-lg font-light tracking-tight text-(--primary-text) tablet:w-xl w-full  tablet:pt-10'>They are exclusively available on our RESELLORA generator. You definitely want to check it out!</p>
                 <PrimaryButton />
             </div>
             <div className="flex flex-col h-full gap-10">
-                <div className="relative w-fit h-fit">
+                <div className="relative w-full h-fit tablet:w-fit">
                     <iframe className="tablet:w-xl tablet:h-96 w-full h-72" src="https://www.youtube.com/embed/kQzavUaxAh8" title="StockX Receipt Generator by HYPECEIPT" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     <div className="absolute inset-0 m-auto h-100 w-90 rounded-full bg-(--accent-text) opacity-40 blur-[100px] -z-10" />
                 </div>
