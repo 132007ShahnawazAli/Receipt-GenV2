d2dcd92cdb993abe264afa2d9585d190adc6ace1
Shahnawaz Ali <shahnawaz132007@gmail.com>
2025-03-25 00:51:42 +0530

    Affiliate and Delivery pages for improved readability

--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -4,8 +4,8 @@ import React from 'react'
 function Reviews() {
     return (
         <div className="w-full flex tablet:flex-row flex-col gap-10 tablet:gap-0 justify-between tablet:pt-10 ">
-            <h1 className=' font-semibold text-(--primary-text) text-5xl tablet:w-1/2 tablet:text-7xl tracking-tighter tablet:leading-20 drop-shadow-[0px_0px_50px_var(--accent-text)]'>REVIEWS</h1>
-            <p className='tablet:text-2xl text-lg font-light tracking-tight text-(--primary-text) tablet:w-xl w-full pt-10'>Coming Soon</p>
+            <h1 className=' font-semibold text-(--primary-text) text-5xl tablet:w-1/2 tablet:text-7xl tracking-tighter tablet:leading-20'>REVIEWS</h1>
+            <p className='tablet:text-2xl text-lg font-semibold tracking-tight text-(--primary-text) tablet:w-xl w-full pt-10'>Coming Soon</p>
         </div>
     )
 }
